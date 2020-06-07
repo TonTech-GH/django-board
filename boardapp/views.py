7a159c276ece38f1fe880de14452d49483d2d107
TonTech-GH <idecchimctv+github@gmail.com>
2020-06-07 10:55:46 +0900

    ログイン画面を追加

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,20 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 def signupfunc(request):
+    '''
+    ユーザー登録画面
+    '''
     if request.method == 'POST':
-        posted_username = request.POST['username']
-        posted_password = request.POST['password']
+        username = request.POST['username']
+        password = request.POST['password']
         try:
             # ユーザー名の重複確認
-            User.objects.get(username=posted_username)
+            User.objects.get(username=username)
             return render(request, 'signup.html', {'error': 'このユーザーは登録されています'})
         except:
             # 重複がなかったのでDBに登録する
-            user = User.objects.create_user(posted_username, '', posted_password)
+            user = User.objects.create_user(username, '', password)
             print('****** registered *****')
             print(user.__dict__)
             print('***********************')
             return render(request, 'signup.html', {'some':100})
 
     return render(request, 'signup.html', {'some':100})
+
+def loginfunc(request):
+    '''
+    ログイン画面
+    '''
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # ユーザー名/パスワードの認証を行い、紐づくユーザー情報を持ってくる
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            # ログインセッション生成
+            login(request, user)
+            return redirect('signup')
+        else:
+            return render(request, 'login.html', {'error': 'ログインに失敗しました'})
+
+    return render(request, 'login.html')
