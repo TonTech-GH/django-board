9bba3d0d7c933b02c11ca1e21f2c97ac89952cc5
TonTech-GH <idecchimctv+github@gmail.com>
2020-06-07 10:13:20 +0900

    ユーザー登録時のユーザー名重複判定を追加

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 
-# Create your views here.
-
 def signupfunc(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = User.objects.create_user(username, '', password)
-        return render(request, 'signup.html', {'some':100})
+        posted_username = request.POST['username']
+        posted_password = request.POST['password']
+        try:
+            # ユーザー名の重複確認
+            User.objects.get(username=posted_username)
+            return render(request, 'signup.html', {'error': 'このユーザーは登録されています'})
+        except:
+            # 重複がなかったのでDBに登録する
+            user = User.objects.create_user(posted_username, '', posted_password)
+            print('****** registered *****')
+            print(user.__dict__)
+            print('***********************')
+            return render(request, 'signup.html', {'some':100})
 
     return render(request, 'signup.html', {'some':100})
